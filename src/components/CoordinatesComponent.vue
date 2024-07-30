880f6afc6d93bf129b14ec55957c61efe1320e0b
iSergio97 <sergiotb15@gmail.com>
2024-07-30 13:39:52 +0200

    Reorganización, la dimensión ahora aparece encima del título y se añade un espacio para que la fila de la tarjeta tenga un espacio común en toda la fila

--- src/components/CoordinatesComponent.vue
+++ src/components/CoordinatesComponent.vue
@@ -1,17 +1,17 @@
 <template>
   <div
-    class="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700"
+    class="flex flex-col max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700"
   >
-    <a>
-      <h3 class="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">
-        {{ coord.alias }}
-      </h3>
-    </a>
-    <a>
+    <div>
       <h5 class="mb-2 text-xl font-semibold tracking-tight text-gray-900 dark:text-white">
         {{ coord.dimension }}
       </h5>
-    </a>
+    </div>
+    <div class="flex-grow">
+      <h3 class="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">
+        {{ alias }}
+      </h3>
+    </div>
     <div class="mb-3 font-normal text-gray-500 dark:text-gray-400">
       <p>
         X: <span class="red"> {{ coord.x }}</span>
@@ -29,27 +29,29 @@
         >)
       </p>
     </div>
-    <a
-      class="inline-flex font-medium items-center text-blue-600 hover:underline"
-      @click="openEditModal()"
-    >
-      Editar
-      <svg
-        class="w-3 h-3 ms-2.5 rtl:rotate-[270deg]"
-        aria-hidden="true"
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 18 18"
+    <div class="block">
+      <div
+        class="inline-flex font-medium items-center text-blue-600 hover:underline"
+        @click="openEditModal()"
       >
-        <path
-          stroke="currentColor"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          stroke-width="2"
-          d="M15 11v4.833A1.166 1.166 0 0 1 13.833 17H2.167A1.167 1.167 0 0 1 1 15.833V4.167A1.166 1.166 0 0 1 2.167 3h4.618m4.447-2H17v5.768M9.111 8.889l7.778-7.778"
-        />
-      </svg>
-    </a>
+        Editar
+        <svg
+          class="w-3 h-3 ms-2.5 rtl:rotate-[270deg]"
+          aria-hidden="true"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 18 18"
+        >
+          <path
+            stroke="currentColor"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M15 11v4.833A1.166 1.166 0 0 1 13.833 17H2.167A1.167 1.167 0 0 1 1 15.833V4.167A1.166 1.166 0 0 1 2.167 3h4.618m4.447-2H17v5.768M9.111 8.889l7.778-7.778"
+          />
+        </svg>
+      </div>
+    </div>
 
     <ModalComponent :coord="coord" @close="closeEditModal()" v-if="dialogOpenModal" />
   </div>
@@ -57,7 +59,7 @@
 
 <script setup lang="ts">
 import type { Coordinates } from '@/interfaces/coordinates.interface';
-import { ref, toRefs } from 'vue';
+import { computed, ref, toRefs } from 'vue';
 import ModalComponent from './ModalComponent.vue';
 
 interface Props {
@@ -70,6 +72,13 @@ const props = defineProps<Props>();
 const emit = defineEmits<Emits>();
 
 const { coord } = toRefs(props);
+
+const alias = computed(() => {
+  const { alias } = coord.value;
+  return alias === '' ? 'Sin alias' : alias;
+});
+
+console.log(coord);
 
 let dialogOpenModal = ref(false);
 
